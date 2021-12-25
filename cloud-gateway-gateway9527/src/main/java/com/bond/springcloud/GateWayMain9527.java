a84f6e5719f4ee7fd3771758668fb46971c74042
stephen <1920098158@qq.com>
2021-12-25 14:37:55 +0800

    update

---
+++ cloud-gateway-gateway9527/src/main/java/com/bond/springcloud/GateWayMain9527.java
@@ -0,0 +1,18 @@
+package com.bond.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author Stephen Zhang
+ * @Date 2021/12/25 13:59
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
