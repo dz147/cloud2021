898750c0aef39b6b4172aae1299a59c795153212
stephen <1920098158@qq.com>
2022-01-08 15:30:53 +0800

    update

---
+++ cloud-gateway-gateway9527/src/test/java/com/bond/springcloud/Test.java
@@ -0,0 +1,15 @@
+package com.bond.springcloud;
+
+import java.time.ZonedDateTime;
+
+/**
+ * @Author Stephen Zhang
+ * @Date 2022/1/8 15:05
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        ZonedDateTime time = ZonedDateTime.now();
+        System.out.println(time);
+    }
+}
