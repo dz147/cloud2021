558d60f70b537d70637b4896bd00ed231241771f
stephen <1920098158@qq.com>
2022-01-08 18:47:14 +0800

    add springCloud-Config

---
+++ cloud-config-center-3344/src/main/java/com/bond/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.bond.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author Stephen Zhang
+ * @Date 2022/1/8 16:51
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
---
+++ cloud-config-center-3355/src/main/java/com/bond/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,18 @@
+package com.bond.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author Stephen Zhang
+ * @Date 2022/1/8 18:18
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
