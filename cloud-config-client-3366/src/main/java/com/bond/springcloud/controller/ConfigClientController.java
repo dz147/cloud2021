0ae50639aa277956f04d4f738f31419f8557390a
dz147 <1920098158@qq.com>
2022-01-11 14:54:24 +0800

    add 3366

---
+++ cloud-config-client-3366/src/main/java/com/bond/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.bond.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Stephen
+ * @date: 2022-01-11 14:52
+ */
+@RestController
+@Slf4j
+@RefreshScope
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
