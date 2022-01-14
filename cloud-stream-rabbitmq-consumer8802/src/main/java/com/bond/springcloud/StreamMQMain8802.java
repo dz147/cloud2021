8ce6fdcf84696df74323d38d4993cdb774c11282
dz147 <1920098158@qq.com>
2022-01-14 11:44:48 +0800

    update

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/bond/springcloud/StreamMQMain8802.java
@@ -0,0 +1,17 @@
+package com.bond.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: Stephen
+ * @date: 2022-01-14 11:02
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class, args);
+    }
+}
---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/bond/springcloud/controller/ReceiveMessageListenerController.java
@@ -0,0 +1,27 @@
+package com.bond.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: Stephen
+ * @date: 2022-01-14 11:03
+ */
+@Component
+@EnableBinding(Sink.class)
+@Slf4j
+public class ReceiveMessageListenerController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message) {
+        log.info("接收MQ消息内容：{},消费微服务端口：{}", message.getPayload(), serverPort);
+    }
+}
